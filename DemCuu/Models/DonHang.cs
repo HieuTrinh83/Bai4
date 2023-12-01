f248f57b816b541ef895a5d42e941742e6be5b9b
Trịnh Quang Hiếu <ihdesigne@gmail.com>
2023-12-02 01:30:06 +0700

    temp

---
+++ DemCuu/Models/DonHang.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemCuu.Models
+{
+    public class DonHang
+    {
+        public short Status { get; set; }
+
+        public int SoLuong { get; set; }
+
+        public DateTime ThoiGianDat { get; set; }
+
+        public DateTime ThoiGianHoanThien { get; set; }
+    }
+}
